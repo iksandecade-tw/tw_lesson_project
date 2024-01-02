8a7f2f5dea19e2f9f9f602c9e9f850aede5f1080
Ihksan Sukmawan <>
2024-01-02 09:12:05 +0700

    add main page

--- lib/presentation/pages/splash_page.dart
+++ lib/presentation/pages/splash_page.dart
@@ -10,14 +10,90 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const PlatformAppBar(
-        title: Text(
-          'Starting Page',
-        ),
-      ),
       body: Padding(
         padding: UIHelper.padding(horizontal: 20, vertical: 10),
-        child: const Text('starting project'),
+        child: ListView(
+          children: [
+            MenuItemWidget(
+                title: 'Dummy UI',
+                description: 'Introducing basic flutter widget UI',
+                onItemTapped: () =>
+                    AutoRouter.of(context).pushNamed('/dummyUI')),
+            UIHelper.divider(),
+            const MenuItemWidget(
+              title: 'Counter Example (State Management)',
+              description:
+                  'Introducing of state management using flutter_bloc. Level: 1',
+            ),
+            UIHelper.divider(),
+            const MenuItemWidget(
+              title: 'Input Validation Example (State Management)',
+              description:
+                  'Flutter state management using flutter_bloc to handle validation in text field. Level: 2',
+            ),
+            UIHelper.divider(),
+            const MenuItemWidget(
+              title: 'Calculator Example (State Management)',
+              description:
+                  'Flutter state management using flutter_bloc to calculate simple syntax. Level: 3',
+            ),
+            UIHelper.divider(),
+            const MenuItemWidget(
+              title: 'News App',
+              description: 'API calling using free source from NYTimes',
+            ),
+            UIHelper.divider(),
+            const MenuItemWidget(
+              title: 'To Do App',
+              description:
+                  'Create a to do list that saved in local storage using hydrated_bloc. Level: 4',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MenuItemWidget extends StatelessWidget {
+  final String title;
+  final String description;
+  final Function()? onItemTapped;
+  const MenuItemWidget(
+      {super.key,
+      required this.title,
+      required this.description,
+      this.onItemTapped});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onItemTapped,
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(
+                      fontSize: 16,
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  description,
+                  style: const TextStyle(fontSize: 12, color: Colors.grey),
+                )
+              ],
+            ),
+          ),
+          const Icon(
+            Icons.chevron_right,
+            size: 36,
+          )
+        ],
       ),
     );
   }
